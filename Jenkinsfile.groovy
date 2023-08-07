3effb1e3549b6c97f2bf3e24b371b7a53274022d
guziqiang <guziqiang@hupu.com>
2023-08-07 10:46:14 +0800

    多节点执行deploy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,45 +18,46 @@ timestamps {
     try {
         echo "---------开始执行--------"
 
-        node('build') {
-            checkout scm
-
-            stage('checkout') {
-                git_checkout()
-            }
-
-            stage('build') {
-                sh "${params.BUILD_SCRIPT}"
-            }
-
-            stage('upload') {
-                sh "~/aws-bin/aws s3 cp ${params.BUILD_PATH}/target/${JOB_NAME}-exec.jar s3://hoorah-deploy/${params.ENV}/${JOB_NAME}/${BUILD_NUMBER}/"
-                stash(name: 'app', includes: "${params.BUILD_PATH}/target/${JOB_NAME}-exec.jar")
-            }
-        }
+//        node('build') {
+//            checkout scm
+//
+//            stage('checkout') {
+//                git_checkout()
+//            }
+//
+//            stage('build') {
+//                sh "${params.BUILD_SCRIPT}"
+//            }
+//
+//            stage('upload') {
+//                sh "~/aws-bin/aws s3 cp ${params.BUILD_PATH}/target/${JOB_NAME}-exec.jar s3://hoorah-deploy/${params.ENV}/${JOB_NAME}/${BUILD_NUMBER}/"
+//                stash(name: 'app', includes: "${params.BUILD_PATH}/target/${JOB_NAME}-exec.jar")
+//            }
+//        }
 
         stage('deploy') {
             script {
                 for (node in get_node("${JOB_NAME}-${params.ENV}")) {
-                    node("${node}") {
-                        unstash "app"
-
-                        sh """
-                        # 创建目录
-                        mkdir -pv /data/www-data
-                        mkdir -pv /data0/log-data/${JOB_NAME}
-
-                        # 设置文件和目录的所有者为 www 用户
-                        chown www.www -R /data0
-                        chown www.www -R /data/www-data
-
-                        # 复制文件并覆盖同名文件
-                        cp -f ${params.BUILD_PATH}/target/${JOB_NAME}-exec.jar /data/www-data/${JOB_NAME}.jar
-
-                        # 重新启动服务
-                        sudo systemctl restart ${JOB_NAME}
-                        """
-                    }
+                    echo "Node ${node.name} has the desired label."
+//                    node("${node}") {
+//                        unstash "app"
+//
+//                        sh """
+//                        # 创建目录
+//                        mkdir -pv /data/www-data
+//                        mkdir -pv /data0/log-data/${JOB_NAME}
+//
+//                        # 设置文件和目录的所有者为 www 用户
+//                        chown www.www -R /data0
+//                        chown www.www -R /data/www-data
+//
+//                        # 复制文件并覆盖同名文件
+//                        cp -f ${params.BUILD_PATH}/target/${JOB_NAME}-exec.jar /data/www-data/${JOB_NAME}.jar
+//
+//                        # 重新启动服务
+//                        sudo systemctl restart ${JOB_NAME}
+//                        """
+//                    }
                 }
             }
         }
